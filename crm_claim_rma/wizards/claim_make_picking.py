65d18ebb58225afa96f3645221e2478612d68b85
George Daramouskas <gdaramouskas@therp.nl>
2018-05-23 12:05:15 +0200

    [FIX] Claim make picking wizard is shown full screen, change treeview for the claim lines shown on the wizard

--- crm_claim_rma/wizards/claim_make_picking.py
+++ crm_claim_rma/wizards/claim_make_picking.py
@@ -109,7 +109,8 @@ class ClaimMakePicking(models.TransientModel):
 
         move_field = 'move_in_id' if picking_type == 'in' else 'move_out_id'
         domain = [
-            ('claim_id', '=', self.env.context['active_id']),
+            ('claim_id', '=', self.env.context.get('active_id') or \
+            self.env.context.get('claim_id')),
             ('product_id.type', '!=', 'service'),
         ]
         lines = self.env['claim.line'].\
@@ -210,7 +211,10 @@ class ClaimMakePicking(models.TransientModel):
             picking_type = warehouse_rec.int_type_id
             write_field = 'move_out_id'
 
-        claim = self.env['crm.claim'].browse(self.env.context['active_id'])
+        claim = self.env['crm.claim'].browse(
+            self.env.context.get('active_id') or self.env.context.get(
+                'claim_id')
+        )
         partner_id = claim.delivery_address_id.id
         claim_lines = self.claim_line_ids
 
